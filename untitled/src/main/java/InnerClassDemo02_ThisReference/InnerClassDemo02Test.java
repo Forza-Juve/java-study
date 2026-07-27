a68be3f9dd26e5d558a1ea9350a9d5d624f5a48c
Juve <363442783@qq.com>
2026-07-27 16:38:12 +0800

    day29 6.3.1-6.3.2  内部类练习Demo

---
+++ untitled/src/main/java/InnerClassDemo02_ThisReference/InnerClassDemo02Test.java
@@ -0,0 +1,10 @@
+package InnerClassDemo02_ThisReference;
+
+public class InnerClassDemo02Test {
+    public static void main(String[] args) {
+        Outer outer = new Outer("外部类");
+        Outer.Inner inner = outer.createInner("内部类");
+        inner.showInner();
+        inner.compareObject();
+    }
+}
---
+++ untitled/src/main/java/InnerDemo01_InnerClassAccess/Computer.java
@@ -0,0 +1,30 @@
+package InnerDemo01_InnerClassAccess;
+
+public class Computer {
+    private String brand;
+    private int price;
+
+    public Computer(String brand, int price) {
+        this.brand = brand;
+        this.price = price;
+    }
+
+    class CPU{
+        private String type;
+
+        public CPU(String type) {
+            this.type = type;
+        }
+
+        public void showInfo(){
+            System.out.println("电脑品牌：" + Computer.this.brand + "\n"
+                    + "电脑价格：" + Computer.this.price + "\n" +
+                    "CPU：" + this.type);
+        }
+
+        public void printObject(){
+            System.out.println(Computer.this.getClass().getName());
+            System.out.println(this.getClass().getName());
+        }
+    }
+}
